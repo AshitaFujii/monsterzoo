2ecf6a202243c99a9759fb2748a6a92cfe1cac07
藤井朝 <ashitaf@AsaBook-Pro.local>
2022-11-03 11:46:08 +0900

    配列をArrayListに置き換える 処理をメソッドに置き換える クラスを作って変数をまとめる

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 
 public class Main {
 	static MonsterZoo pz = new MonsterZoo();
@@ -26,42 +26,38 @@ public class Main {
 
 		System.out.println("ボールがなくなった！");
 
-		for(int i=0;i<pz.getUserMonster().length;i++){
-			if(pz.getUserMonster()[i]!=null){
-				System.out.println(pz.getUserMonster()[i]+"を捕まえた．");
-			}
+		for(Monster mon : pz.getUserMonster()){
+			System.out.println(mon.name+"を捕まえた．");
 		}
 	}
 
 	//テスト用のモンスターデータを登録するメソッド
 	public static void setMonsterZukan(){
-		String tempMonster[] = new String[22];
-		double tempMonsterRare[] = new double[22];
-		tempMonster[0]="イガキン";	tempMonsterRare[0]=9;
-		tempMonster[1]="ナマチュウ";	tempMonsterRare[1]=3;
-		tempMonster[2]="イノウエン";	tempMonsterRare[2]=1;
-		tempMonster[3]="リョージィ";	tempMonsterRare[3]=2;
-		tempMonster[4]="アキモトン";	tempMonsterRare[4]=5;
-		tempMonster[5]="ゴージマ";	tempMonsterRare[5]=4;
-		tempMonster[6]="チュウデン";	tempMonsterRare[6]=6;
-		tempMonster[7]="ヅカホン";	tempMonsterRare[7]=8;
-		tempMonster[8]="ニシムラー";	tempMonsterRare[8]=7;
-		tempMonster[9]="サコーデン";	tempMonsterRare[9]=2;
-		tempMonster[10]="ウッチー";	tempMonsterRare[10]=5;
-		tempMonster[11]="ハヤッシー";	tempMonsterRare[11]=4;
-		tempMonster[12]="キーチー";	tempMonsterRare[12]=3;
-		tempMonster[13]="リョクン";	tempMonsterRare[13]=1;
-		tempMonster[14]="デコポン";	tempMonsterRare[14]=6;
-		tempMonster[15]="カミサン";	tempMonsterRare[15]=5;
-		tempMonster[16]="シスイ";	tempMonsterRare[16]=1;
-		tempMonster[17]="ジョナ";	tempMonsterRare[17]=7;
-		tempMonster[18]="ギダギダ";	tempMonsterRare[18]=2;
-		tempMonster[19]="ミッツー";	tempMonsterRare[19]=8;
-		tempMonster[20]="ゾエサン";	tempMonsterRare[20]=5;
-		tempMonster[21]="キタバー";	tempMonsterRare[21]=3;
+		ArrayList<Monster> monsterZukan = new ArrayList<Monster>();
+		monsterZukan.add(new Monster("イガキン", 9));
+		monsterZukan.add(new Monster("ナマチュウ", 3));
+		monsterZukan.add(new Monster("イノウエン", 1));
+		monsterZukan.add(new Monster("リョージィ", 2));
+		monsterZukan.add(new Monster("アキモトン", 5));
+		monsterZukan.add(new Monster("ゴージマ", 4));
+		monsterZukan.add(new Monster("チュウデン", 6));
+		monsterZukan.add(new Monster("ヅカホン", 8));
+		monsterZukan.add(new Monster("ニシムラー", 7));
+		monsterZukan.add(new Monster("サコーデン", 2));
+		monsterZukan.add(new Monster("ウッチー", 5));
+		monsterZukan.add(new Monster("ハヤッシー", 4));
+		monsterZukan.add(new Monster("キーチー", 3));
+		monsterZukan.add(new Monster("リョクン", 1));
+		monsterZukan.add(new Monster("デコポン", 6));
+		monsterZukan.add(new Monster("カミサン", 5));
+		monsterZukan.add(new Monster("シスイ", 1));
+		monsterZukan.add(new Monster("ジョナ", 7));
+		monsterZukan.add(new Monster("ギダギダ", 2));
+		monsterZukan.add(new Monster("ミッツー", 8));
+		monsterZukan.add(new Monster("ゾエサン", 5));
+		monsterZukan.add(new Monster("キタバー", 3));
 
-		pz.setMonsterZukan(tempMonster);
-		pz.setMonsterRare(tempMonsterRare);
+		pz.setMonsterZukan(monsterZukan);
 	}
 
 }
